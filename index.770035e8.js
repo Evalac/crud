91f06e1a5a24db4592ba8fc166400dcedc4df792
Evalac <102866941+Evalac@users.noreply.github.com>
2024-08-29 17:39:21 +0000

    Deploying to gh-pages from @ Evalac/crud@e423d09867b498e88a8f6036c8a32bdcceb23fd4 🚀

---
+++ index.770035e8.js
@@ -0,0 +1,2 @@
+fetch("http://localhost:3000/books").then((o=>o.json())),fetch(`http://localhost:3000/books/${4}`).then((o=>o.json()));
+//# sourceMappingURL=index.770035e8.js.map
---
+++ index.92b6da77.js
@@ -0,0 +1,2 @@
+fetch("http://localhost:3000/books").then((function(t){return t.json()})),fetch("http://localhost:3000/books/".concat(4)).then((function(t){return t.json()}));
+//# sourceMappingURL=index.92b6da77.js.map
