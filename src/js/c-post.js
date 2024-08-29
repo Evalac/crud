e4c422aa7c564806a8d10eb347ffda2031dd20d3
Evalac <maslenokprograming@gmail.com>
2024-08-29 21:46:41 +0300

    post

--- src/js/c-post.js
+++ src/js/c-post.js
@@ -1,24 +1,23 @@
 const BASE_URL = `http://localhost:3000`;
 
 const newBook = {
-  title: 'Test',
-  author: 'Me',
+  title: 'Test1',
+  author: 'Me2',
   genres: ['Classic', 'Romance'],
   rating: 4.9,
 };
 
-const options = {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify(newBook),
-};
-
-function fetchPostNewBook() {
+function fetchPostNewBook(newBook) {
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newBook),
+  };
   fetch(`http://localhost:3000/books`, options)
     .then(responce => responce.json())
     .then(console.log);
 }
 
-fetchPostNewBook();
+fetchPostNewBook(newBook);
