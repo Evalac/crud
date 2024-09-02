501b9d09ca0f6045aacd77f2a38f410c32e4eaab
Evalac <102866941+Evalac@users.noreply.github.com>
2024-09-02 17:09:48 +0000

    Deploying to gh-pages from @ Evalac/crud@9232384f2f950afd47110614fe81b60b39abcf94 🚀

--- index.445be246.js
+++
@@ -1,2 +0,0 @@
-var e;(e="apple",Promise.resolve({strawberry:"полинця",kiwi:"kiwi",apple:"apl"}[e])).then(console.log);
-//# sourceMappingURL=index.445be246.js.map
--- index.d260e783.js
+++
@@ -1,2 +0,0 @@
-!function(){var e;(e="apple",Promise.resolve({strawberry:"полинця",kiwi:"kiwi",apple:"apl"}[e])).then(console.log)}();
-//# sourceMappingURL=index.d260e783.js.map
