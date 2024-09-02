81cab34b51e0046e0536799cb22cd21e88014f12
Evalac <maslenokprograming@gmail.com>
2024-09-02 17:26:44 +0300

    commit

--- src/js/async.js
+++ src/js/async.js
@@ -1,13 +1,47 @@
-setTimeout(() => {
-  try {
-    console.log(`в середині try до myvar`);
-    myVar;
-    console.log(`після  myVar`);
-  } catch (error) {
-    console.log(error);
+// {
+//   setTimeout(() => {
+//     try {
+//       console.log(`в середині try до myvar`);
+//       myVar;
+//       console.log(`після  myVar`);
+//     } catch (error) {
+//       console.log(error);
 
-    console.log(`помилка `);
-  }
-}, 1000);
+//       console.log(`помилка `);
+//     }
+//   }, 1000);
 
-console.log(`після try... catch`);
+//   console.log(`після try... catch`);
+// }
+
+////////////////////////////////////////
+// {
+//   const valiJSON = '{"Name":"Mango","age":3}';
+//   const invalidJSON = '{бекенд повернув таке чудо}';
+
+//   try {
+//     console.log(1);
+
+//     console.log(JSON.parse(invalidJSON));
+//   } catch (error) {
+//     if (error.name === 'SyntaxError') {
+//       console.log('Do it anything');
+//     }
+//     console.log(error);
+//   }
+
+//   console.log(2);
+// }
+
+///////////////////////////////////
+function getFruit(name) {
+  const fruiyts = {
+    strawberry: 'полинця',
+    kiwi: 'kiwi',
+    apple: 'apl',
+  };
+
+  return Promise.resolve(fruiyts[name]);
+}
+
+getFruit('apple').then(console.log);
