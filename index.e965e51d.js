6ca8faa8c8688e42dc9e906780bc523703334ef1
Evalac <102866941+Evalac@users.noreply.github.com>
2024-09-03 18:14:10 +0000

    Deploying to gh-pages from @ Evalac/crud@844f249d0eb197552d7f98e8b2ecb4b45dff8218 🚀

--- index.e965e51d.js
+++
@@ -1,2 +0,0 @@
-const t={title:"DODO",author:"Me2",genres:["Classic","Romance"],rating:4.9};!async function(o){try{const o=await async function(t){const o={method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify(t)},n=await fetch("http://locsalhost:3000/books",o);return await n.json()}(t);console.log(o)}catch(t){console.log(t)}}();
-//# sourceMappingURL=index.e965e51d.js.map
