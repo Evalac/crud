dcf9a2d46252aac7b5a8c5b03e6ffe703c6906ce
Evalac <102866941+Evalac@users.noreply.github.com>
2024-09-03 12:07:21 +0000

    Deploying to gh-pages from @ Evalac/crud@9f958789fc09e97e2c4f79a8825586975fd7e65e 🚀

--- index.b24cf691.js
+++
@@ -1,2 +0,0 @@
-function e(e){const t={strawberry:"🍓",kiwi:"🥝",apple:"🍏"};return new Promise(((r,n)=>setTimeout((()=>{r(t[e]),n("reject")}),500)))}(async function(){try{const t=e("kiwi"),r=e("strawberry"),n=e("apple");return await Promise.all([n,t,r])}catch(e){console.log("Помилка"+e)}})().then((e=>console.log(e)));
-//# sourceMappingURL=index.b24cf691.js.map
