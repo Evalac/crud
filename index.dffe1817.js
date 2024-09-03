91435238ecbf05cf64c972ef5270398ef85b6642
Evalac <102866941+Evalac@users.noreply.github.com>
2024-09-03 11:35:50 +0000

    Deploying to gh-pages from @ Evalac/crud@8c4335571c04640bc86e0c6d96c1b63f4b1def42 🚀

--- index.dffe1817.js
+++
@@ -1,2 +0,0 @@
-function e(e){return Promise.resolve({strawberry:"полинця",kiwi:"kiwi",apple:"apl"}[e])}e("apple").then((o=>{console.log(o),e("kiwi").then((e=>console.log(e))),e("strawberry").then((e=>console.log(e)))}));
-//# sourceMappingURL=index.dffe1817.js.map
