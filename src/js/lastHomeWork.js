2de80f16c000bd2dcceb9cb707f99a81a4256cfd
Maslenok.M <102866941+Evalac@users.noreply.github.com>
2024-09-11 17:19:46 +0300

    need add load moreBTN

--- src/js/lastHomeWork.js
+++ src/js/lastHomeWork.js
@@ -1,9 +1,8 @@
+const { error, log } = require('console');
 const { release } = require('process');
 
 const axios = require('axios').default;
 const BASE_URL = `https://pixabay.com/api/`;
-
-const search = `cat`;
 
 const options = {
   key: `45910491-7a91b10438fcd735159f6d92e`,
@@ -13,22 +12,78 @@ const options = {
   safesearch: true,
 };
 
-async function getSearchPixabay(evt) {
+const refs = {
+  formEl: document.querySelector('#search-form'),
+  galarryEl: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('.load-more'),
+};
+
+refs.formEl.addEventListener('submit', onSearch);
+refs.loadMoreBtn.addEventListener('click', loadMore);
+
+function loadMore(evt) {}
+
+function onSearch(evt) {
+  evt.preventDefault();
+  const formData = new FormData(evt.currentTarget);
+  const inputValue = formData.get('searchQuery');
+  const data = getSearchPixabay(inputValue);
+
+  data
+    .then(data => {
+      console.log(data);
+      createMarkup([data]);
+    })
+    .catch(error => console.log(error))
+    .finally();
+}
+
+async function getSearchPixabay(inputValue) {
   const response = await axios.get(
-    `${BASE_URL}?key=45910491-7a91b10438fcd735159f6d92e&q=${search}&image_type=photo&orientation=horizontal&safesearch=true`
+    `${BASE_URL}?key=45910491-7a91b10438fcd735159f6d92e&q=${inputValue}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40`
   );
+  if (response.ok) {
+    throw new Error(response.status);
+  }
   return response;
 }
 
-getSearchPixabay()
-  .then(function (response) {
-    // обробка успішного запиту
-    console.log(response);
-  })
-  .catch(function (error) {
-    // обробка помилки
-    console.log(error);
-  })
-  .finally(function () {
-    // виконується завжди
-  });
+function createMarkup(data) {
+  const markup = data.map(({ data: { hits } }) =>
+    hits
+      .map(
+        ({
+          webformatURL,
+          largeImageURL,
+          tags,
+          likes,
+          views,
+          comments,
+          downloads,
+        }) => `<div class="photo-card">
+  <img src="${webformatURL}" alt="" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>
+      ${likes}
+    </p>
+    <p class="info-item">
+      <b>Views</b>
+      ${views}
+    </p>
+    <p class="info-item">
+      <b>Comments</b>
+      ${comments}
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>
+      ${downloads}
+    </p>
+  </div>
+</div>`
+      )
+      .join('')
+  );
+
+  return (refs.galarryEl.innerHTML = markup);
+}
